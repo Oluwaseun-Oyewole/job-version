6f9b0dad9e622b240fa23aa6619a73d581d593e8
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2025-08-30 20:29:54 +0100

    fix: deploy bug

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,16 @@
+import { auth } from "@/auth";
 import JobsTable from "@/components/JobsTable";
+import prisma from "@/lib/prisma";
 import { isServerTokenValid } from "@/utils/helper";
 import { routes } from "@/utils/routes";
 import { redirect } from "next/navigation";
-import { getJobs } from "../actions/action";
 
 export default async function postJob() {
-  const jobs = await getJobs();
+  const session = await auth();
+  const jobs = await prisma.jobs.findMany({
+    orderBy: { created_at: "desc" },
+    where: { user: { email: session?.user?.email } },
+  });
   if (!(await isServerTokenValid())) return redirect(routes.login);
   return <JobsTable jobs={jobs} />;
 }
